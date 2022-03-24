10d4b5a2615cea1a8f23370213a5629e4db5f90e
Vampi29 <aleroblesdm@gmail.com>
2022-03-23 23:36:02 -0300

    Proctfile y app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(3000, ()=>{
+app.listen(process.nav.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
 
